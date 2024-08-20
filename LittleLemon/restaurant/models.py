8bee6f7692332b24c5ff89c45901c9e833172661
Gary <caozhengchen@gmail.com>
2024-08-20 14:25:24 +0800

    update 240820

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10,decimal_places=2)
+    Inventory = models.IntegerField(validators=[MinValueValidator(0)])
+
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(6)])
+    BookingDate = models.DateTimeField()
+
